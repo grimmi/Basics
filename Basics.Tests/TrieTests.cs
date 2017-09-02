f60c98d7773569549607a558a0627c4335b6b7d7
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-02 19:42:28 +0200

    first steps in a trie implementation

---
+++ Basics.Tests/TrieTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basics.Tests
+{
+    [TestFixture]
+    public class TrieTests
+    {
+        private class TestTrie : Trie
+        {
+            public TrieNode HeadNode => Head;
+        }
+
+        [Test]
+        public void Insert_SingleLetter_TrieContainsOneEdgeAndTwoItems()
+        {
+            var trie = new TestTrie();
+            trie.Insert("a");
+
+            Assert.AreEqual(1, trie.HeadNode.OutEdges.Count);
+            Assert.AreEqual("a", trie.HeadNode.OutEdges.First().Value);
+        }
+
+        [Test]
+        public void Insert_OneStringWithTwoLetters_TrieContainsOnePathWithThreeNodes()
+        {
+            var trie = new TestTrie();
+            trie.Insert("ab");
+
+            Assert.AreEqual(1, trie.HeadNode.OutEdges.Count);
+            Assert.AreEqual("a", trie.HeadNode.OutEdges.First().Value);
+
+            var aNode = trie.HeadNode.OutEdges.First().To;
+            Assert.NotNull(aNode);
+            Assert.AreEqual("a", aNode.Value);
+            Assert.AreEqual(1, aNode.OutEdges.Count);
+            Assert.AreEqual("b", aNode.OutEdges.First().Value);
+
+            var bNode = aNode.OutEdges.First().To;
+            Assert.NotNull(bNode);
+            Assert.AreEqual("ab", bNode.Value);
+        }
+    }
+}
---
+++ Basics/Trie.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Basics
+{
+    public class Trie
+    {
+        public class TrieEdge
+        {
+            public TrieNode From { get; }
+            public TrieNode To { get; }
+
+            public string Value { get; }
+            public TrieEdge(TrieNode from, string value)
+            {
+                Value = value;
+                From = from;
+                To = new TrieNode(From.GetPath().Concat(new[] { this }));
+            }
+        }
+
+        public class TrieNode
+        {
+            public string Value { get; }
+            public TrieEdge InEdge { get; }
+            public List<TrieEdge> OutEdges { get; } = new List<TrieEdge>();
+
+            public TrieNode(IEnumerable<TrieEdge> edges)
+            {
+                Value = string.Concat(edges.Select(e => e.Value));
+                InEdge = edges.Any() ? edges.Last() : null;
+            }
+
+            public IEnumerable<TrieEdge> GetPath()
+            {
+                var edge = InEdge;
+                var path = new List<TrieEdge>();
+                while(edge?.From != null)
+                {
+                    path.Add(edge);
+                    edge = edge.From.InEdge;
+                }
+
+                return path.AsEnumerable().Reverse();
+            }
+        }
+
+        protected TrieNode Head = new TrieNode(Enumerable.Empty<TrieEdge>());
+
+        public Trie()
+        {
+
+        }
+
+        public Trie(IEnumerable<string> values)
+        {
+            foreach(var value in values)
+            {
+                Insert(Head, value);
+            }
+        }
+
+        public void Insert(string value)
+        {
+            Insert(Head, value);
+        }
+
+        protected virtual void Insert(TrieNode node, string value)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            if(node.OutEdges.Any(e => value.StartsWith(e.Value)))
+            {
+                Insert(node.OutEdges.First(e => value.StartsWith(e.Value)).To, value.Substring(1));
+            }
+            else
+            {
+                var newEdge = new TrieEdge(node, value.Substring(0, 1));
+                node.OutEdges.Add(newEdge);
+                Insert(newEdge.To, value.Substring(1));
+            }
+        }
+    }
+}
