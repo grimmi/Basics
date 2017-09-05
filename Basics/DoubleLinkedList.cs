bb8845fff30d42a08622e23d321ed73ded3eb7d6
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-05 21:02:31 +0200

    first simple version of a double linked list

---
+++ Basics/DoubleLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics
+{
+    public class DoubleLinkedList<T> where T : IComparable<T>
+    {
+        protected DoubleLinkNode<T> Head { get; set; }
+        protected DoubleLinkNode<T> Foot { get; set; }
+
+        public void InsertAtEnd(T value)
+        {
+            if(Head == null)
+            {
+                Head = Foot = new DoubleLinkNode<T>(value);
+                return;
+            }
+
+            Foot.Next = new DoubleLinkNode<T>(value);
+            Foot = Foot.Next;
+        }
+
+        public void InsertInOrder(T value)
+        {
+            if(Head == null)
+            {
+                Head = Foot = new DoubleLinkNode<T>(value);
+                return;
+            }
+
+            var node = Head;
+            while(node != null && node.Value.CompareTo(value) < 0)
+            {
+                node = node.Next;
+            }
+
+            if(node == null)
+            {
+                InsertAtEnd(value);
+                return;
+            }
+
+            var newNode = new DoubleLinkNode<T>(value);
+            newNode.Previous = node;
+            newNode.Next = node.Next;
+            node.Next.Previous = newNode;
+            node.Next = newNode;
+        }
+    }
+}
