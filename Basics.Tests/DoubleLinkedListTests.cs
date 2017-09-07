03d6744d934b6774d9574f7281ab1c0ce292c884
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-07 20:16:28 +0200

    added one more test

--- Basics.Tests/DoubleLinkedListTests.cs
+++ Basics.Tests/DoubleLinkedListTests.cs
@@ -28,5 +28,16 @@ namespace Basics.Tests
 
             Assert.AreEqual(3, list.Count());
         }
+
+        [Test]
+        public void InsertInOrder_SameElementTwice_ShouldReturnBoth()
+        {
+            var list = new DoubleLinkedList<int>();
+            list.InsertInOrder(2);
+            list.InsertInOrder(2);
+
+            Assert.AreEqual(2, list.Count());
+            CollectionAssert.AreEqual(new[] { 2, 2 }, list.ToArray());
+        }
     }
 }
